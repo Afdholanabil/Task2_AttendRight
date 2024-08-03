9de3f4b4f870933631f241cd7a2350ed5164b0d4
Afdholanabil <afdholanabil41435@gmail.com>
2024-08-03 20:32:57 +0700

    editing login

---
+++ app/src/main/java/com/example/task2_attendright/presentation/customview/MyButton.kt
@@ -0,0 +1,34 @@
+package com.example.task2_attendright.presentation.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.example.task2_attendright.R
+
+
+class MyButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatButton(context, attrs) {
+
+    private var txtColor: Int = 0
+    private var enabledBackground: Drawable
+    private var disabledBackground: Drawable
+    init {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_login) as Drawable
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_login_disable) as Drawable
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = if(isEnabled) enabledBackground else disabledBackground
+        setTextColor(txtColor)
+        textSize = 16f
+        gravity = Gravity.CENTER
+        text = if(isEnabled) context.getString(R.string.sigin) else context.getString(R.string.sigin_butuh_input)
+    }
+}
--- app/src/main/java/com/example/task2_attendright/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/task2_attendright/presentation/ui/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.task2_attendright.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.task2_attendright.R
+import com.example.task2_attendright.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding :ActivityMainBinding? = null
+    private val _binding get() = binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding!!.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding!!.tvTitleSlashScreen.alpha = 0f
+        binding!!.tvTitleSlashScreen.animate().setDuration(3000).alpha(1f).withEndAction{
+            val i = Intent(this, LoginWEmailActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
